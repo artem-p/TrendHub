0c2366d1a0726b28f20a86bfcd0d2321bf7f1513
artem-p <artempugachev1@gmail.com>
2018-07-02 15:21:39 +0300

    add projects cache interface

---
+++ data/src/main/java/ru/artempugachev/data/repository/ProjectsCache.kt
@@ -0,0 +1,28 @@
+package ru.artempugachev.data.repository
+
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import ru.artempugachev.data.model.ProjectEntity
+
+interface ProjectsCache {
+
+    fun clearProjects(): Completable
+
+    fun saveProjects(projects: List<ProjectEntity>): Completable
+
+    fun getProjects(): Observable<List<ProjectEntity>>
+
+    fun getBookmarkedProjects(): Observable<List<ProjectEntity>>
+
+    fun setProjectAsBookmarked(projectId: String): Completable
+
+    fun setProjectAsNotBookmarked(projectId: String): Completable
+
+    fun areProjectsCached(): Single<Boolean>
+
+    fun setLastCacheTime(lastCache: Long): Completable
+
+    fun isProjectsCacheExpired(): Single<Boolean>
+
+}
