e92f2f89c633005c36cf1ffa64d4028b7100b60e
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-07 19:59:45 +0300

    add test factory class for creating projects and projects entity

---
+++ data/src/test/java/ru/artempugachev/data/test/factory/ProjectFactory.kt
@@ -0,0 +1,25 @@
+package ru.artempugachev.data.test.factory
+
+import ru.artempugachev.data.model.ProjectEntity
+import ru.artempugachev.domain.model.Project
+
+class ProjectFactory {
+
+    fun makeProjectEntity(): ProjectEntity {
+        return ProjectEntity(DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomBoolean())
+    }
+
+
+    fun makeProject(): Project {
+        return Project(DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomString(), DataFactory.randomString(),
+                DataFactory.randomBoolean())
+    }
+
+}
