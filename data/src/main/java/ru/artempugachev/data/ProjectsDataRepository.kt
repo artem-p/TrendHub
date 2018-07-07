b47baba3e52a3216d5c90ede50b8c7d41b7f5e63
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-07 19:41:07 +0300

    implement projects data repository

--- data/src/main/java/ru/artempugachev/data/ProjectsDataRepository.kt
+++ data/src/main/java/ru/artempugachev/data/ProjectsDataRepository.kt
@@ -2,24 +2,53 @@ package ru.artempugachev.data
 
 import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import ru.artempugachev.data.mapper.ProjectMapper
+import ru.artempugachev.data.repository.ProjectsCache
+import ru.artempugachev.data.store.ProjectsDataStoreFactory
 import ru.artempugachev.domain.model.Project
 import ru.artempugachev.domain.repository.ProjectsRepository
 import javax.inject.Inject
 
-class ProjectsDataRepository @Inject constructor(): ProjectsRepository {
+class ProjectsDataRepository @Inject constructor(
+        private val mapper: ProjectMapper,
+        private val cache: ProjectsCache,
+        private val factory: ProjectsDataStoreFactory)
+    : ProjectsRepository {
     override fun getProjects(): Observable<List<Project>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        return Observable.zip(cache.areProjectsCached().toObservable(),
+                cache.isProjectsCacheExpired().toObservable(),
+                BiFunction<Boolean, Boolean, Pair<Boolean, Boolean>> { areCached, isExpired ->
+                    Pair(areCached, isExpired)
+                })
+                .flatMap {
+                    factory.getDataStore(it.first, it.second).getProjects()
+                }
+                .flatMap { projects ->
+                    factory.getCacheDataStore()
+                            .saveProjects(projects)
+                            .andThen(Observable.just(projects))
+                }
+                .map {
+                    it.map {
+                        mapper.mapFromEntity(it)
+                    }
+                }
     }
 
     override fun bookmarkProject(projectId: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return factory.getCacheDataStore().setProjectAsBookmarked(projectId)
     }
 
     override fun unbookmarkProject(projectId: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return factory.getCacheDataStore().setProjectAsNotBookmarked(projectId)
     }
 
     override fun getBookmarkedProjects(): Observable<List<Project>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return factory.getCacheDataStore().getBookmarkedProjects()
+                .map {
+                    it.map { mapper.mapFromEntity(it) }
+                }
     }
 }
