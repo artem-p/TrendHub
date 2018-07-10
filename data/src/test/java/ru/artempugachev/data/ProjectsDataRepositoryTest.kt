62057814a9d91e45b215c3a64b44f9b149d83a22
artem-p <artempugachev1@gmail.com>
2018-07-10 11:47:29 +0300

    test get projects complete

--- data/src/test/java/ru/artempugachev/data/ProjectsDataRepositoryTest.kt
+++ data/src/test/java/ru/artempugachev/data/ProjectsDataRepositoryTest.kt
@@ -5,6 +5,7 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import ru.artempugachev.data.mapper.ProjectMapper
@@ -12,6 +13,8 @@ import ru.artempugachev.data.model.ProjectEntity
 import ru.artempugachev.data.repository.ProjectsCache
 import ru.artempugachev.data.repository.ProjectsDataStore
 import ru.artempugachev.data.store.ProjectsDataStoreFactory
+import ru.artempugachev.data.test.factory.ProjectFactory
+import ru.artempugachev.domain.model.Project
 
 
 @RunWith(JUnit4::class)
@@ -30,7 +33,23 @@ class ProjectsDataRepositoryTest {
     }
 
 
-    private fun stubProjects(observable: Observable<List<ProjectEntity>>) {
+    @Test
+    fun getProjectsComplete() {
+        stubGetProjects(Observable.just(listOf(ProjectFactory.makeProjectEntity())))
+        stubMapper(ProjectFactory.makeProject(), any())
+
+        val testObserver = repository.getProjects().test()
+        testObserver.assertComplete()
+    }
+
+
+    private fun stubMapper(model: Project, entity: ProjectEntity) {
+        whenever(mapper.mapFromEntity(entity))
+                .thenReturn(model)
+    }
+
+
+    private fun stubGetProjects(observable: Observable<List<ProjectEntity>>) {
         whenever(store.getProjects())
                 .thenReturn(observable)
     }
