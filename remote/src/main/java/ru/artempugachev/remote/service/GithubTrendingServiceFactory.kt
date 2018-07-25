c186ecb21821cf68802d9569c5cb440547717227
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-25 19:16:26 +0300

    add github service factory

---
+++ remote/src/main/java/ru/artempugachev/remote/service/GithubTrendingServiceFactory.kt
@@ -0,0 +1,54 @@
+package ru.artempugachev.remote.service
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.util.concurrent.TimeUnit
+
+
+class GithubTrendingServiceFactory {
+
+     // todo add Dagger here
+
+
+    fun makeGithubTrendingService(isDebug: Boolean): GithubTrendingService {
+        val okHttpClient = makeOkHttpClient(
+                makeLoggingInterceptor((isDebug)))
+        return makeGithubTrendingService(okHttpClient)
+    }
+
+
+    private fun makeGithubTrendingService(okHttpClient: OkHttpClient): GithubTrendingService {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(GITHUB_BASE_URL)
+                .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        return retrofit.create(GithubTrendingService::class.java)
+    }
+
+
+    private fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .build()
+    }
+
+
+    private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        loggingInterceptor.level = if (isDebug) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+
+        return loggingInterceptor
+    }
+}
+
+const val GITHUB_BASE_URL = "https://api.github.com/"
