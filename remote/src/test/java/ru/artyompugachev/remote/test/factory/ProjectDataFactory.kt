25bdf6a4ed08922a32b7479bbeaca6294a30d37d
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-29 18:15:32 +0300

    add remote test factory classes

---
+++ remote/src/test/java/ru/artyompugachev/remote/test/factory/ProjectDataFactory.kt
@@ -0,0 +1,33 @@
+package ru.artyompugachev.remote.test.factory
+
+import ru.artempugachev.data.model.ProjectEntity
+import ru.artempugachev.remote.model.OwnerModel
+import ru.artempugachev.remote.model.ProjectModel
+import ru.artempugachev.remote.model.ProjectsResponseModel
+
+class ProjectDataFactory {
+
+    fun makeOwner(): OwnerModel {
+        return OwnerModel(DataFactory.randomUuid(), DataFactory.randomUuid())
+    }
+
+
+    fun makeProject(): ProjectModel {
+        return ProjectModel(DataFactory.randomUuid(), DataFactory.randomUuid(),
+                DataFactory.randomUuid(), DataFactory.randomInt(),
+                DataFactory.randomUuid(), makeOwner())
+    }
+
+
+    fun makeProjectEntity(): ProjectEntity {
+        return ProjectEntity(DataFactory.randomUuid(), DataFactory.randomUuid(),
+                DataFactory.randomUuid(), DataFactory.randomUuid(),
+                DataFactory.randomUuid(), DataFactory.randomUuid(),
+                DataFactory.randomUuid())
+    }
+
+
+    fun makeProjectsResponse(): ProjectsResponseModel {
+        return ProjectsResponseModel(listOf(makeProject(), makeProject()))
+    }
+}
