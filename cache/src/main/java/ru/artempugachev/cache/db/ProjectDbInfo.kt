3a4e984a96785305a9dbb4b65ceec0f9f2a84122
Artyom Pugachev <artempugachev1@gmail.com>
2018-08-04 19:19:11 +0300

    add column names

--- cache/src/main/java/ru/artempugachev/cache/db/ProjectDbInfo.kt
+++ cache/src/main/java/ru/artempugachev/cache/db/ProjectDbInfo.kt
@@ -4,4 +4,10 @@ object ProjectDbInfo {
     const val TABLE_NAME = "projects"
     const val COLUMN_PROJECT_ID = "id"
     const val COLUMN_IS_BOOKMARKED = "is_bookmarked"
+    const val COLUMN_NAME = "name"
+    const val COLUMN_FULL_NAME = "full_name"
+    const val COLUMN_STAR_COUNT = "star_count"
+    const val COLUMN_DATE_CREATED = "date_created"
+    const val COLUMN_OWNER_NAME = "owner_name"
+    const val COLUMN_OWNER_AVATAR = "owner_avatar"
 }
--- cache/src/main/java/ru/artempugachev/cache/model/CachedProject.kt
+++ cache/src/main/java/ru/artempugachev/cache/model/CachedProject.kt
@@ -8,12 +8,25 @@ import ru.artempugachev.cache.db.ProjectDbInfo
 @Entity(tableName = ProjectDbInfo.TABLE_NAME)
 data class CachedProject (
     @PrimaryKey
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_PROJECT_ID)
     var id: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_NAME)
     var name: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_FULL_NAME)
     var fullName: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_STAR_COUNT)
     var starCount: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_DATE_CREATED)
     var dateCreated: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_OWNER_NAME)
     var ownerName: String,
+
+    @ColumnInfo(name = ProjectDbInfo.COLUMN_OWNER_AVATAR)
     var ownerAvatar: String,
 
     @ColumnInfo(name = ProjectDbInfo.COLUMN_IS_BOOKMARKED)
