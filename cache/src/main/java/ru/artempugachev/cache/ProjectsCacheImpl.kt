61460ae02d0e6c372c0cdf72e5b2dd5eec1bf07c
Artyom Pugachev <artempugachev1@gmail.com>
2018-08-05 11:21:18 +0300

    add projects cache implementation stub

---
+++ cache/src/main/java/ru/artempugachev/cache/ProjectsCacheImpl.kt
@@ -0,0 +1,52 @@
+package ru.artempugachev.cache
+
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import ru.artempugachev.cache.db.ProjectsDatabase
+import ru.artempugachev.cache.mapper.CachedProjectMapper
+import ru.artempugachev.data.model.ProjectEntity
+import ru.artempugachev.data.repository.ProjectsCache
+import javax.inject.Inject
+
+class ProjectsCacheImpl @Inject constructor(
+        private val projectsDatabase: ProjectsDatabase,
+        private val mapper: CachedProjectMapper)
+    :ProjectsCache {
+
+    override fun clearProjects(): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saveProjects(projects: List<ProjectEntity>): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<ProjectEntity>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setProjectAsBookmarked(projectId: String): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setProjectAsNotBookmarked(projectId: String): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun areProjectsCached(): Single<Boolean> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setLastCacheTime(lastCache: Long): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isProjectsCacheExpired(): Single<Boolean> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
