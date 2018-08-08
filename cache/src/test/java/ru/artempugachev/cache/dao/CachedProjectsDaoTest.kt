cde9689791ab5fae22cf0139b8f7325f8ad6dad4
artem-p <artempugachev1@gmail.com>
2018-08-08 15:20:38 +0300

    add cached projects dao test

---
+++ cache/src/test/java/ru/artempugachev/cache/dao/CachedProjectsDaoTest.kt
@@ -0,0 +1,88 @@
+package ru.artempugachev.cache.dao
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.persistence.room.Room
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import ru.artempugachev.cache.db.ProjectsDatabase
+import ru.artempugachev.cache.factory.ProjectDataFactory
+
+
+@RunWith(RobolectricTestRunner::class)
+class CachedProjectsDaoTest {
+
+    @Rule
+    @JvmField var instantTaskTestRule = InstantTaskExecutorRule()
+
+    private val database = Room.inMemoryDatabaseBuilder(
+            RuntimeEnvironment.application.applicationContext,
+            ProjectsDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+
+    @Test
+    fun getProjectsReturnsData() {
+        val project = ProjectDataFactory.makeCachedProject()
+        database.cachedProjectsDao().insertProjects(listOf(project))
+
+        val testObserver = database.cachedProjectsDao().getProjects().test()
+        testObserver.assertValue(listOf(project))
+    }
+
+
+    @Test
+    fun deleteProjectsClearsData() {
+        val project = ProjectDataFactory.makeCachedProject()
+        database.cachedProjectsDao().insertProjects(listOf(project))
+        database.cachedProjectsDao().deleteProjects()
+
+        val testObserver = database.cachedProjectsDao().getProjects().test()
+        testObserver.assertValue(emptyList())
+    }
+
+
+    @Test
+    fun getBookmarkedProjectsReturnsData() {
+        val project = ProjectDataFactory.makeCachedProject()
+        val bookmarkedProject = ProjectDataFactory.makeBookmarkedCachedProject()
+        database.cachedProjectsDao().insertProjects(listOf(project, bookmarkedProject))
+
+        val testObserver = database.cachedProjectsDao().getBookmarkedProjects().test()
+        testObserver.assertValue(listOf(bookmarkedProject))
+    }
+
+
+    @Test
+    fun setProjectAsBookmarkedSavesData() {
+        val project = ProjectDataFactory.makeCachedProject()
+        database.cachedProjectsDao().insertProjects(listOf(project))
+        database.cachedProjectsDao().setBookmarkStatus(true, project.id)
+        project.isBookmarked = true
+
+        val testObserver = database.cachedProjectsDao().getBookmarkedProjects().test()
+        testObserver.assertValue(listOf(project))
+    }
+
+
+    @Test
+    fun setProjectAsNotBookmarkedSavesData() {
+        val project = ProjectDataFactory.makeBookmarkedCachedProject()
+        database.cachedProjectsDao().insertProjects(listOf(project))
+        database.cachedProjectsDao().setBookmarkStatus(false, project.id)
+        project.isBookmarked = false
+
+        val testObserver = database.cachedProjectsDao().getBookmarkedProjects().test()
+        testObserver.assertValue(emptyList())
+    }
+}
