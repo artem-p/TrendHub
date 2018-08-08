50eebb424131af7fb13792ecaf51f96f0cfbb2e5
artem-p <artempugachev1@gmail.com>
2018-08-08 16:01:41 +0300

    begin projects cache impl test

---
+++ cache/src/test/java/ru/artempugachev/cache/ProjectsCacheImplTest.kt
@@ -0,0 +1,67 @@
+package ru.artempugachev.cache
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.persistence.room.Room
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import ru.artempugachev.cache.db.ProjectsDatabase
+import ru.artempugachev.cache.factory.ProjectDataFactory
+import ru.artempugachev.cache.mapper.CachedProjectMapper
+
+
+@RunWith(RobolectricTestRunner::class)
+class ProjectsCacheImplTest {
+
+    @get:Rule var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val database = Room.inMemoryDatabaseBuilder(
+            RuntimeEnvironment.application.applicationContext,
+            ProjectsDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    private val entityMapper = CachedProjectMapper()
+    private val cache = ProjectsCacheImpl(database, entityMapper)
+
+
+    @Test
+    fun clearProjectsCompletes() {
+        val testObserver = cache.clearProjects().test()
+
+        testObserver.assertComplete()
+    }
+
+
+    @Test
+    fun saveProjectsCompletes() {
+        val projects = listOf(ProjectDataFactory.makeProjectEntity())
+
+        val testObserver = cache.saveProjects(projects).test()
+        testObserver.assertComplete()
+    }
+
+
+    @Test
+    fun getProjectsReturnsData() {
+        val projects = listOf(ProjectDataFactory.makeProjectEntity())
+        cache.saveProjects(projects).test()
+
+        val testObserver = cache.getProjects().test()
+        testObserver.assertValue(projects)
+    }
+
+
+    @Test
+    fun getBookmarkedProjectsReturnsData() {
+        val bookmarkedProject = ProjectDataFactory.makeBookmarkedProjectEntity()
+        val projects = listOf(ProjectDataFactory.makeProjectEntity(),
+                bookmarkedProject)
+        cache.saveProjects(projects).test()
+
+        val testObserver = cache.getBookmarkedProjects().test()
+        testObserver.assertValue(listOf(bookmarkedProject))
+    }
+
+}
