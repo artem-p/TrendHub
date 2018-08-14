b97a0a65a0f71b201c154b7e6552462d0726d488
artem-p <artempugachev1@gmail.com>
2018-08-14 16:10:47 +0300

    add project view mapper test

---
+++ presentation/src/test/java/ru/artempugachev/presentation/test/mapper/ProjectViewMapperTest.kt
@@ -0,0 +1,31 @@
+package ru.artempugachev.presentation.test.mapper
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import ru.artempugachev.presentation.mapper.ProjectViewMapper
+import ru.artempugachev.presentation.test.factory.ProjectFactory
+
+
+@RunWith(JUnit4::class)
+class ProjectViewMapperTest {
+
+    private val mapper = ProjectViewMapper()
+
+    @Test
+    fun mapToViewMapsData() {
+        val project = ProjectFactory.makeProject()
+        val projectView = mapper.mapToView(project)
+
+        assertEquals(project.id, projectView.id)
+        assertEquals(project.name, projectView.name)
+        assertEquals(project.fullName, projectView.fullName)
+        assertEquals(project.starCount, projectView.starCount)
+        assertEquals(project.dateCreated, projectView.dateCreated)
+        assertEquals(project.ownerName, projectView.ownerName)
+        assertEquals(project.ownerAvatar, projectView.ownerAvatar)
+        assertEquals(project.isBookmarked, projectView.isBookmarked)
+    }
+
+}
