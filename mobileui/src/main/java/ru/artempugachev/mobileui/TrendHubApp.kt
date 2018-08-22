ff2dd19857b3a3abe5cbe7a95b6619f23619b6c7
artem-p <artempugachev1@gmail.com>
2018-08-22 13:13:03 +0300

    inject app component in application class

--- mobileui/src/main/java/ru/artempugachev/mobileui/TrendHubApp.kt
+++ mobileui/src/main/java/ru/artempugachev/mobileui/TrendHubApp.kt
@@ -1,14 +1,32 @@
 package ru.artempugachev.mobileui
 
+import android.app.Activity
 import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import ru.artempugachev.mobileui.di.DaggerAppComponent
 import timber.log.Timber
+import javax.inject.Inject
 
-class TrendHubApp : Application() {
+class TrendHubApp : Application(), HasActivityInjector {
+    @Inject lateinit var androidInjector: DispatchingAndroidInjector<Activity>
+
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return androidInjector
+    }
 
     override fun onCreate() {
         super.onCreate()
 
         setUpTimber()
+
+        DaggerAppComponent
+                .builder()
+                .application(this)
+                .build()
+                .inject(this)
     }
 
 
