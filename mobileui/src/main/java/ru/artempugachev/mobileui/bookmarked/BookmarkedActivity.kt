3aaa4819b329067706d13cf73a504a78293b897f
artem-p <artempugachev1@gmail.com>
2018-08-27 17:19:36 +0300

    add bookmarked activity

---
+++ mobileui/src/main/java/ru/artempugachev/mobileui/bookmarked/BookmarkedActivity.kt
@@ -0,0 +1,85 @@
+package ru.artempugachev.mobileui.bookmarked
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import dagger.android.AndroidInjection
+import ru.artempugachev.mobileui.R
+import ru.artempugachev.mobileui.di.ViewModelFactory
+import ru.artempugachev.presentation.BrowseBookmarkedProjectsViewModel
+import ru.artempugachev.presentation.mapper.ProjectViewMapper
+import ru.artempugachev.presentation.model.ProjectView
+import ru.artempugachev.presentation.state.Resource
+import ru.artempugachev.presentation.state.ResourceState
+import kotlinx.android.synthetic.main.activity_bookmarked.*
+import javax.inject.Inject
+
+class BookmarkedActivity : AppCompatActivity() {
+
+    @Inject lateinit var mapper: ProjectViewMapper
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+    lateinit var browseViewModel: BrowseBookmarkedProjectsViewModel
+
+    companion object {
+        fun getStartIntent(context: Context): Intent {
+            return Intent(context, BookmarkedActivity::class.java)
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmarked)
+
+        AndroidInjection.inject(this)
+
+        browseViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(BrowseBookmarkedProjectsViewModel::class.java)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setupBrowseRecycler()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        browseViewModel.getProjects().observe(this,
+                Observer<Resource<List<ProjectView>>> {
+                    it?.let {
+                        handleDataState(it)
+                    }
+                })
+        browseViewModel.fetchProjects()
+    }
+
+
+    private fun setupBrowseRecycler() {
+        recycler_projects.layoutManager = LinearLayoutManager(this)
+        recycler_projects.adapter = adapter
+    }
+
+
+    private fun handleDataState(resource: Resource<List<ProjectView>>) {
+        when (resource.status) {
+            ResourceState.SUCCESS -> {
+                progress.visibility = View.GONE
+                recycler_projects.visibility = View.VISIBLE
+                resource.data?.let {
+                    adapter.projects = it.map { mapper.mapToView(it) }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            ResourceState.LOADING -> {
+                progress.visibility = View.VISIBLE
+                recycler_projects.visibility = View.GONE
+            }
+        }
+    }
+}
